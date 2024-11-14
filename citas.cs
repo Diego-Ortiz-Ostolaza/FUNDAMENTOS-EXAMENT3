796b74e273d2f17391ff59a55d20a3a079445b2c
Diego-Ortiz-Ostolaza <diego.ortiz.joseph@gmail.com>
2024-11-14 08:37:03 -0500

    CITAS Y ESTUDIANTE

---
+++ citas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_EXAMEN
+{
+    internal class citas
+    {
+        public int numero {  get; set; }
+        public estudiante estudiante { get; set; }
+        public string enfermedad {  get; set; }
+        
+        public double precio { get; set; }
+        public override string ToString()
+        {
+            return $"{numero}:\n{estudiante.nombre} de la universidad {estudiante.universidad} con codigo de alumno{estudiante.codigo}, tendrá una cita médica por tener {enfermedad}. \n La cita costará {precio}";
+        }
+        public void agregar() { 
+
+        }
+    }
+}
---
+++ estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_EXAMEN
+{
+    internal class estudiante
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string universidad { get; set; }
+
+        
+        public override string ToString()
+        {
+            return $"El estudiante {nombre}, estudia en la universidad {universidad} y tiene como código de alumno {codigo}"; 
+        }
+    }
+}
